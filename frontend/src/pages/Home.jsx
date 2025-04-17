b42d5abe42bacf538ee253c0074f9b1d5a1ed8f6
AngelMoreno24 <angel.moreno8424@gmail.com>
2025-04-16 18:44:28 -0700

    the expenses are now seperated into different arrays for the specific category

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,6 +6,17 @@ const Home = () => {
   const [expenses, setExpenses] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const [food, setFood] = useState([]);
+  const [transportation, setTransportation] = useState([]);
+  const [shopping, setShopping] = useState([]);
+  const [bills, setBills] = useState([]);
+  const [travel, setTravel] = useState([]);
+  const [healthcare, setHealthcare] = useState([]);
+  const [entertainment, setEntertainment] = useState([]);
+  const [education, setEducation] = useState([]);
+  const [gifts, setGifts] = useState([]);
+  const [misc, setMisc] = useState([]);
+
   const fetchExpenses = () => {
     setLoading(true);
     fetch('http://localhost:3000/api/expenses', {
@@ -29,6 +40,63 @@ const Home = () => {
     fetchExpenses();
   }, []);
 
+  useEffect(() => {
+    categorizeExpenses();
+  }, [expenses]);
+  const categorizeExpenses = () => {
+    expenses.map(e => {
+
+      if (e.category == "food") {
+        setFood(prev => [...prev, e]);
+      }
+
+      if (e.category == "transportation") {
+        setTransportation(prev => [...prev, e]);
+      }
+
+      if (e.category == "shopping") {
+        setShopping(prev => [...prev, e]);
+      }
+
+      if (e.category == "bills") {
+        setBills(prev => [...prev, e]);
+      }
+
+      if (e.category == "travel") {
+        setTravel(prev => [...prev, e]);
+      }
+
+      if (e.category == "healthcare") {
+        setHealthcare(prev => [...prev, e]);
+      }
+      if (e.category == "entertainment") {
+        setEntertainment(prev => [...prev, e]);
+      }
+
+      if (e.category == "education") {
+        setEducation(prev => [...prev, e.category]);
+      }
+
+      if (e.category == "gifts") {
+        setGifts(prev => [...prev, e.category]);
+      }
+
+      if (e.category == "misc") {
+        setMisc(prev => [...prev, e.category]);
+      }
+
+      // food: ['zomato'
+      //transportation:
+      //shopping: ['ama
+      //bills: ['electr
+      //travel: ['hotel
+      //healthcare: ['h
+      //entertainment: 
+      //education: ['co
+      //gifts: ['gift',
+      //misc: ['miscell
+    });
+  }
   return (
     <div>
       <h2>Your Expenses</h2>
@@ -38,8 +106,8 @@ const Home = () => {
 
       {loading ? (
         <p>Loading...</p>
-      ) : expenses.length > 0 ? (
-        expenses.map((e, i) => (
+      ) : shopping.length > 0 ? (
+        shopping.map((e, i) => (
           <div key={i}>
             <p>{e.snippet}</p>
             <strong>Amount: ${e.amount}</strong>
